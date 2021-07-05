f269e9b034bbcf00c64ce9eccef9015601a51ca3
Андрей Беляков <ttxiom@gmail.com>
2021-07-05 15:22:54 +0500

    up

---
+++ Practice/Practice/FormStart.Designer.cs
@@ -0,0 +1,138 @@
+﻿
+namespace Practice
+{
+    partial class FormStart
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.btnAuto = new System.Windows.Forms.Button();
+            this.btnReg = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.status1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 1;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.Controls.Add(this.btnAuto, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.btnReg, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.statusStrip1, 0, 2);
+            this.tableLayoutPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(0, 0);
+            this.tableLayoutPanel1.Margin = new System.Windows.Forms.Padding(2);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 3;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 50F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Absolute, 23F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(509, 258);
+            this.tableLayoutPanel1.TabIndex = 0;
+            // 
+            // btnAuto
+            // 
+            this.btnAuto.AutoEllipsis = true;
+            this.btnAuto.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnAuto.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnAuto.ForeColor = System.Drawing.Color.Teal;
+            this.btnAuto.Location = new System.Drawing.Point(29, 131);
+            this.btnAuto.Margin = new System.Windows.Forms.Padding(29, 14, 29, 29);
+            this.btnAuto.Name = "btnAuto";
+            this.btnAuto.Size = new System.Drawing.Size(451, 74);
+            this.btnAuto.TabIndex = 0;
+            this.btnAuto.Text = "Авторизация";
+            this.btnAuto.UseVisualStyleBackColor = true;
+            this.btnAuto.Click += new System.EventHandler(this.btnAuto_Click);
+            // 
+            // btnReg
+            // 
+            this.btnReg.AutoEllipsis = true;
+            this.btnReg.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.btnReg.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.btnReg.ForeColor = System.Drawing.Color.Teal;
+            this.btnReg.Location = new System.Drawing.Point(29, 29);
+            this.btnReg.Margin = new System.Windows.Forms.Padding(29, 29, 29, 14);
+            this.btnReg.Name = "btnReg";
+            this.btnReg.Size = new System.Drawing.Size(451, 74);
+            this.btnReg.TabIndex = 0;
+            this.btnReg.Text = "Регистрация";
+            this.btnReg.UseVisualStyleBackColor = true;
+            this.btnReg.Click += new System.EventHandler(this.btnReg_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.status1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 236);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(1, 0, 16, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(509, 22);
+            this.statusStrip1.TabIndex = 1;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // status1
+            // 
+            this.status1.ForeColor = System.Drawing.SystemColors.Highlight;
+            this.status1.Name = "status1";
+            this.status1.Size = new System.Drawing.Size(79, 17);
+            this.status1.Text = "Соединение";
+            // 
+            // FormStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(509, 258);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Margin = new System.Windows.Forms.Padding(2);
+            this.MaximumSize = new System.Drawing.Size(702, 446);
+            this.MinimumSize = new System.Drawing.Size(352, 270);
+            this.Name = "FormStart";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Учёт тем ВКР";
+            this.Load += new System.EventHandler(this.FormStart_Load);
+            this.Shown += new System.EventHandler(this.FormStart_Shown);
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+        private System.Windows.Forms.Button btnReg;
+        private System.Windows.Forms.Button btnAuto;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel status1;
+    }
+}
+
---
+++ Practice/Practice/FormStart.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+//using MySql.Data.MySqlClient;
+
+namespace Practice
+{
+    public partial class FormStart : Form
+    {
+        public FormStart()
+        {
+            InitializeComponent();
+        }
+
+        private void btnReg_Click(object sender, EventArgs e)
+        {
+			
+        }
+		
+        private void formClosed(object sender, FormClosedEventArgs e)
+        {
+            
+        }
+
+        private void btnAuto_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void FormStart_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void FormStart_Shown(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void CheckConnection(Task obj)
+        {
+            
+        }
+    }
+}
